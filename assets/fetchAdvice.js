7950bebc09336b214579c95a05b6bca91c903fc5
Oxana2332 <far_north@ro.ru>
2024-11-25 14:43:49 +0300

    ещё одна попытка  в БЭМ

--- assets/fetchAdvice.js
+++ assets/fetchAdvice.js
@@ -1,9 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', function (event) {
 	fetch('https://api.adviceslip.com/advice')
 		.then(response => response.json())
 		.then(slip => {
-			document.getElementById('advice').innerText = slip.slip.advice;
+			document.getElementById('advice__text').innerText =
+				slip.slip.advice;
 		})
 		.catch(error => console.log(error));
 });
